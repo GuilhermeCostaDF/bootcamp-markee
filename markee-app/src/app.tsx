d3476c0fc77da539bff466800c8a3bb450ef70d1
GuidoCostaDF <guidocostadf@gmail.com>
2022-09-27 10:19:47 -0300

    config tema styled components

---
+++ markee-app/src/app.tsx
@@ -0,0 +1,15 @@
+import styled, { css } from 'styled-components'
+
+function App () {
+  return (
+    <Title>App</Title>
+  )
+}
+
+const Title = styled.h1`${({ theme }) => css`
+  background: ${theme.colors.black};
+  color: ${theme.colors.primary};
+  padding: 10px;
+`}
+`
+export { App }
---
+++ markee-app/src/root.tsx
@@ -0,0 +1,13 @@
+import { App } from './app'
+import { ThemeProvider } from 'styled-components'
+import { theme } from 'resources/theme'
+
+function Root () {
+  return (
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  )
+}
+
+export { Root }
