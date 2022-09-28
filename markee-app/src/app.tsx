368841372519ccf7232870dad86434ef46859b38
GuidoCostaDF <guidocostadf@gmail.com>
2022-09-28 11:04:08 -0300

    update

--- markee-app/src/app.tsx
+++ markee-app/src/app.tsx
@@ -2,9 +2,18 @@ import styled, { css } from 'styled-components'
 
 function App () {
   return (
-    <Title>App</Title>
+    <Title>
+      App {' '}
+      <Span>
+        Texto do span
+      </Span>
+    </Title>
   )
 }
+
+const Span = styled.span`
+  color: ${({theme}) => theme.colors.gray}
+`
 
 const Title = styled.h1`${({ theme }) => css`
   background: ${theme.colors.black};
